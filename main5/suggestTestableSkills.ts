5054fd3d2fda6c400dab85f1f73aa81d83116896
Florian Dietz <you@example.com>
2026-07-28 23:19:34 +0200

    28.07.26

---
+++ main5/suggestTestableSkills.ts
@@ -0,0 +1,50 @@
+
+
+const converge0 = (a: number) => (val: number) => (a / (val + a));
+const converge1 = (a: number) => (val: number) => 1 - converge0(a)(val);
+
+const forgetDays = converge0(10 /* days until half forgotten */);
+const keepDays = converge1(10);
+
+const keepRepetitions = converge1(3);
+interface Result {beforeDays: number, correctness: number /* [0, 1] */ }
+
+const consciousness = ( orderedResults: Result[]) => 
+
+    keepRepetitions(orderedResults.reduce((sum, {beforeDays, correctness}, idx, arr) => 
+        sum +
+        idx === 0 ? 
+         correctness * forgetDays(beforeDays) 
+         :correctness * forgetDays(beforeDays) * keepDays(arr[idx-1].beforeDays - beforeDays) * arr[idx-1].correctness 
+            * 2 -1 
+         , 0));
+
+const getBeforeDays = (date: Date) => undefined as unknown as number;
+
+const containsAspectOtherAspect = (aspect: string, testParentAspect: string) => testParentAspect.includes(aspect);
+
+const prioritize = (results: {date: Date, aspect: string, correctness: number /* [0, 1] */}[]) => {
+        const aspects = Array.from(new Set(results.map(result => result.aspect)));
+        const consciousnessOfAspects = aspects.map(
+            aspect =>
+            ({
+                aspect,
+                orderedResults: results
+                .filter(result => containsAspectOtherAspect(aspect, result.aspect))
+                .map( result => ({beforeDays: getBeforeDays(result.date), correctness: result.correctness}))
+                .sort((a, b) => a.beforeDays - b.beforeDays )
+            })     
+        ).reduce((acc, {aspect, orderedResults}) =>({...acc, [aspect]: consciousness(orderedResults)}), {} as Record<string, number>);
+   
+        
+        return {
+            1: Object.entries(consciousnessOfAspects).filter(([_, consciousness]) => consciousness < 0.2),
+            2: Object.entries(consciousnessOfAspects).filter(([_, consciousness]) => consciousness >= 0.2 && consciousness < 0.5),
+            3: Object.entries(consciousnessOfAspects).filter(([_, consciousness]) => consciousness >= 0.5 && consciousness < 0.8),
+            4: Object.entries(consciousnessOfAspects).filter(([_, consciousness]) => consciousness > 0.8),
+        }
+        
+   
+    }
+
+const 
