2d9b61eb4bccd6200b76c1081c0efa266895a2ce
Florian Dietz <florian.dietz@posteo.de>
2026-08-07 21:16:07 +0200

    Disable test in main4

--- drafts/main4.test.ts
+++ drafts/main4.test.ts
@@ -1,67 +1,74 @@
-describe('UserManager', () => {
-  describe('getSuggestedAspectsForRepetition', () => {
-    describe('getPriority', () => {
-      it('', () => {
-        const lastRepetition = 'short';
-        const lastResult = 'bad';
+// describe('UserManager', () => {
+//   describe('getSuggestedAspectsForRepetition', () => {
+//     describe('getPriority', () => {
+//       it('', () => {
+//         const lastRepetition = 'short';
+//         const lastResult = 'bad';
 
-        const actual = 'getPriority()';
-        expect(actual).toEqual(1);
-      });
-      it('', () => {
-        const inFocus = true;
-        const lastResult = 'bad';
+//         const actual = 'getPriority()';
+//         expect(actual).toEqual(1);
+//       });
+//       it('', () => {
+//         const inFocus = true;
+//         const lastResult = 'bad';
 
-        const actual = 'getPriority()';
-        expect(actual).toEqual(1);
-      });
-      it('', () => {
-        const lastRepetition = 'long';
-        const lastResult = 'middle';
-        const positiveRepetitions = 'seldom';
+//         const actual = 'getPriority()';
+//         expect(actual).toEqual(1);
+//       });
+//       it('', () => {
+//         const lastRepetition = 'long';
+//         const lastResult = 'middle';
+//         const positiveRepetitions = 'seldom';
 
-        const actual = 'getPriority()';
-        expect(actual).toEqual(3);
-      });
-      it('', () => {
-        const inFocus = true;
-        const lastResult = 'middle';
+//         const actual = 'getPriority()';
+//         expect(actual).toEqual(3);
+//       });
+//       it('', () => {
+//         const inFocus = true;
+//         const lastResult = 'middle';
 
-        const actual = 'getPriority()';
-        expect(actual).toEqual(2);
-      });
-      it('', () => {
-        const lastRepetition = 'short';
-        const lastResult = 'good';
-        const positiveRepetitions = 'often';
+//         const actual = 'getPriority()';
+//         expect(actual).toEqual(2);
+//       });
+//       it('', () => {
+//         const lastRepetition = 'short';
+//         const lastResult = 'good';
+//         const positiveRepetitions = 'often';
 
-        const actual = 'getPriority()';
-        expect(actual).toEqual(5);
-      });
-    });
+//         const actual = 'getPriority()';
+//         expect(actual).toEqual(5);
+//       });
+//     });
 
-    describe('selectAspectsByPriority', () => {
-      // TODO research spaced repetition
-      it('select all of priority 1 if amount less than numberOfTasks', () => {
-        const aspectWithPriority: [Aspect, Priority][] = [];
-        const numberOfTasks: number = 42;
-        expect(true).toEqual(true);
-      });
-      it('fill rest with priority 2', () => {
-        const aspectWithPriority: [Aspect, Priority][] = [];
-        const numberOfTasks: number = 42;
-        expect(true).toEqual(true);
-      });
-      it('', () => {
-        const aspectWithPriority: [Aspect, Priority][] = [];
-        const numberOfTasks: number = 42;
-        expect(true).toEqual(true);
-      });
-      it('', () => {
-        const aspectWithPriority: [Aspect, Priority][] = [];
-        const numberOfTasks: number = 42;
-        expect(true).toEqual(true);
-      });
-    });
+//     describe('selectAspectsByPriority', () => {
+//       // TODO research spaced repetition
+//       it('select all of priority 1 if amount less than numberOfTasks', () => {
+//         const aspectWithPriority: [Aspect, Priority][] = [];
+//         const numberOfTasks: number = 42;
+//         expect(true).toEqual(true);
+//       });
+//       it('fill rest with priority 2', () => {
+//         const aspectWithPriority: [Aspect, Priority][] = [];
+//         const numberOfTasks: number = 42;
+//         expect(true).toEqual(true);
+//       });
+//       it('', () => {
+//         const aspectWithPriority: [Aspect, Priority][] = [];
+//         const numberOfTasks: number = 42;
+//         expect(true).toEqual(true);
+//       });
+//       it('', () => {
+//         const aspectWithPriority: [Aspect, Priority][] = [];
+//         const numberOfTasks: number = 42;
+//         expect(true).toEqual(true);
+//       });
+//     });
+//   });
+// });
+
+describe('Example test suite', () => {
+  it('should pass this example test', () => {
+    const actual = 1 + 1;
+    expect(actual).toEqual(2);
   });
 });
