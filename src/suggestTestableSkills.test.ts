7201e75e531e62c82540740fb7043b58385d947f
Florian Dietz <florian.dietz@posteo.de>
2026-08-07 20:30:56 +0200

    apply prettier

--- src/suggestTestableSkills.test.ts
+++ src/suggestTestableSkills.test.ts
@@ -1,13 +1,10 @@
-import { getConsciousness } from "./suggestTestableSkills";
+import { getConsciousness } from './suggestTestableSkills';
 // import {it, describe expect} from "test"
 
 describe('getConsciousness', () => {
-
-    it('forget', () => {
-
-        const a = getConsciousness([{beforeDays: 0.1, correctness: 1}])
-        const b = getConsciousness([{beforeDays: 1, correctness: 1}])
-        expect(a).toBeGreaterThan(b)
-    })
-
+  it('forget', () => {
+    const a = getConsciousness([{ beforeDays: 0.1, correctness: 1 }]);
+    const b = getConsciousness([{ beforeDays: 1, correctness: 1 }]);
+    expect(a).toBeGreaterThan(b);
+  });
 });
