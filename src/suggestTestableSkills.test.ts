b89fd38d08aca51a56edf975a56c2140f9c3c32c
Florian Dietz <florian.dietz@posteo.de>
2026-08-08 12:39:33 +0200

    added some test cases

--- src/suggestTestableSkills.test.ts
+++ src/suggestTestableSkills.test.ts
@@ -2,8 +2,8 @@ import { getConsciousness } from './suggestTestableSkills';
 
 describe('getConsciousness', () => {
   it('consciousness decreases with increasing time of last repetition', () => {
-    const a = getConsciousness([{ beforeDays: 0.1, correctness: 1 }]);
-    const b = getConsciousness([{ beforeDays: 1, correctness: 1 }]);
+    const a = getConsciousness([{ beforeDays: 1, correctness: 1 }]);
+    const b = getConsciousness([{ beforeDays: 2, correctness: 1 }]);
     expect(a).toBeGreaterThan(b);
     console.log('forget', a, b);
   });
@@ -28,6 +28,53 @@ describe('getConsciousness', () => {
     ]);
     const b = getConsciousness([{ beforeDays: 1, correctness: 1 }]);
     expect(a).toBeGreaterThan(b);
+  });
+  it('consciousness decreases with increasing time of last repetition', () => {
+    const a = getConsciousness([
+      { beforeDays: 1, correctness: 1 },
+      { beforeDays: 2, correctness: 1 },
+    ]);
+    const b = getConsciousness([
+      { beforeDays: 2, correctness: 1 },
+      { beforeDays: 3, correctness: 1 },
+    ]);
+    expect(a).toBeGreaterThan(b);
+    console.log('forget', a, b);
+  });
+  it('consciousness is considered less when the level of correctness is lower no matter the good result before', () => {
+    const a = getConsciousness([
+      { beforeDays: 1, correctness: 1 },
+      { beforeDays: 2, correctness: 1 },
+    ]);
+    const b = getConsciousness([
+      { beforeDays: 1, correctness: 0.5 },
+      { beforeDays: 2, correctness: 1 },
+    ]);
+    expect(a).toBeGreaterThan(b);
+    console.log('worse', a, b);
+  });
+  it('consciousness is considered less when the level of correctness is lower no matter the good result after', () => {
+    const a = getConsciousness([
+      { beforeDays: 1, correctness: 1 },
+      { beforeDays: 2, correctness: 1 },
+    ]);
+    const b = getConsciousness([
+      { beforeDays: 1, correctness: 1 },
+      { beforeDays: 2, correctness: 0.5 },
+    ]);
+    expect(a).toBeGreaterThan(b);
+  });
+  it('consciousness is consiedered less when the last bad result is more recent', () => {
+    const a = getConsciousness([
+      { beforeDays: 1, correctness: 1 },
+      { beforeDays: 5, correctness: 0.5 },
+    ]);
+    const b = getConsciousness([
+      { beforeDays: 1, correctness: 1 },
+      { beforeDays: 2, correctness: 0.5 },
+    ]);
+    expect(a).toBeGreaterThan(b);
+    console.log('worse', a, b);
   });
   it.skip('', () => {
     const a = getConsciousness([{ beforeDays: 0.1, correctness: 1 }]);
