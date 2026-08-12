bd815ccd40fffe63792dd775db35ce8aff2ca5a6
Florian Dietz <florian.dietz@posteo.de>
2026-08-12 22:02:04 +0200

    more generig tests in suggestTestableSkills

--- src/suggestTestableSkills.test.ts
+++ src/suggestTestableSkills.test.ts
@@ -1,3 +1,5 @@
+import { getConsciousness, Result } from './getConsciousness';
+
 const subjects = ['a', 'b', 'c', 'd', 'e'];
 
 class LearnScheduler {
@@ -16,24 +18,98 @@ class LearnScheduler {
   }
 }
 
+const LearnSchedulerTestWrapper = (
+  learningTimes: number[],
+  subjects: string[],
+  doSession: (
+    time: number,
+    index: number,
+    session: ReturnType<LearnScheduler['nextSession']>,
+  ) => Parameters<LearnScheduler['recordResults']>[0],
+  checkSession: (
+    time: number,
+    index: number,
+    consciousnesses: Record<string, number>,
+    session: string[],
+    results: Result[],
+  ) => void,
+  doFinal: (scheduler: LearnScheduler) => void,
+) => {
+  const scheduler = new LearnScheduler(subjects);
+  const results: { id: string; time: number; correctness: number }[] = [];
+  learningTimes.forEach((time, index) => {
+    const sessionResults = doSession(
+      time,
+      index,
+      scheduler.nextSession(10, time),
+    );
+
+    scheduler.recordResults(sessionResults, time);
+
+    Object.entries(sessionResults).forEach(([id, correctness]) =>
+      results.push({ correctness, id, time }),
+    );
+
+    Array.from(new Set(results.map((result) => result.id)))
+      .map(
+        (id) =>
+          [
+            id,
+            results
+              .filter((result) => result.id === id)
+              .map(({ correctness, time: _time }) => ({
+                correctness,
+                beforeDays: time - _time,
+              })),
+          ] as const,
+      )
+      .map(([id, results]) => [id, getConsciousness(results)]);
+  });
+
+  doFinal(scheduler);
+};
+
 describe('LearnScheduler', () => {
-  it('limits number of learning in progress subjects', () => {
-    const subjects = Array.from({ length: 100 }).map((i) => `${i}`);
-    const scheduler = new LearnScheduler(subjects);
+  const learningTimes = Array.from({ length: 10 }).map((_, i) => i);
+  const subjects = Array.from({ length: 100 }).map((_, i) => `${i}`);
 
-    for (let i = 0; i < 10; i++) {
-      const session = scheduler.nextSession(10, i);
-      scheduler.recordResults(
+  it('limits number of learning in progress subjects', () =>
+    LearnSchedulerTestWrapper(
+      learningTimes,
+
+      subjects,
+
+      (time, index, session) =>
         Object.fromEntries(session.map((task) => [task, 0.5])),
-        i,
-      );
 
-      expect(Object.values(scheduler.consciousness).length).toEqual(100);
-      expect(
-        Object.values(scheduler.consciousness).filter(
-          (value) => value > 0.1 && value < 0.8,
-        ).length,
-      ).toBeLessThan(10);
-    }
+      (time, index, consciousnesses) => {
+        expect(Object.values(consciousnesses).length).toEqual(100);
+        expect(
+          Object.values(consciousnesses).filter(
+            (value) => value > 0.1 && value < 0.8,
+          ).length,
+        ).toBeLessThan(10);
+      },
+      () => {},
+    ));
+
+  it('assures constant repetition of currently learnt tasks', () => {
+    let checkForNextSession: string[] = [];
+    LearnSchedulerTestWrapper(
+      learningTimes,
+
+      subjects,
+
+      (time, index, session) =>
+        Object.fromEntries(session.map((task) => [task, 0.5])),
+
+      (time, index, consciousnesses, session) => {
+        expect(checkForNextSession.every((task) => session.includes(task)));
+        checkForNextSession = session.filter(
+          (task) => consciousnesses[task] > 0.1 && consciousnesses[task] < 0.8,
+        );
+      },
+      () => {},
+    );
   });
 });
